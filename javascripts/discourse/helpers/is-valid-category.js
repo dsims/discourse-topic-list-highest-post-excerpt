d88fbe06f7761986ee1abcb781b8b0945a711789
marc <marc.garcia@marfeel.com>
2024-05-06 16:06:09 +0200

    empty array

--- javascripts/discourse/helpers/is-valid-category.js
+++ javascripts/discourse/helpers/is-valid-category.js
@@ -1,9 +1,10 @@
 import { registerRawHelper } from 'discourse-common/lib/helpers';
 
 registerRawHelper('isValidCategory', function(value) {
-	const array = settings.categories.split("|")
+	const array = settings.categories.split('|')
+		.filter((id) => id !== '')
 		.map((id) => parseInt(id, 10))
 		.filter((id) => id);
 
-	return array.includes(value);
+	return array.length === 0 || array.includes(value);
 });
